d0dbcdd10a335d0d5c2e0655f769c8685663ad79
ntahompagazee <zxcdeath1337@gmail.com>
2026-03-02 13:44:57 +0700

    hi

--- src/NoteManager.java
+++ src/NoteManager.java
@@ -1,3 +1,9 @@
 public class NoteManager {
+    public void addNote(String note) {
+        System.out.println("Заметка добавлена: " + note);
+    }
 
+    public void deleteNote(String note) {
+        System.out.println("Заметка удалена: " + note);
+    }
 }
