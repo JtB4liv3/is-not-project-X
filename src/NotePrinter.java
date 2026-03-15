8ffec8c68774b8ee591b090bdb0fdba9bf4b9b88
BogDeepSeek <kkorsakov47@gmail.com>
2026-03-15 17:15:16 +0700

    Cоздана функция printAllNotes и в методе main добавленна вызов этой функции

--- src/NotePrinter.java
+++ src/NotePrinter.java
@@ -8,7 +8,17 @@ public class NotePrinter {
         notes = new ArrayList<>();
 
     }
+
+    public void printAllNotes() {
+        System.out.println("Список всех заметок");
+        if (notes.isEmpty()) {
+            System.out.println("Заметок пока нет.");
+        }
+        
+    }
+
     public static void main(String[] args) {
         NotePrinter printer = new NotePrinter();
+        printer.printAllNotes();
     }
 }
