84bb7ef6de000e45ae61c137d912ce68aaabc8a7
Елизавета Ларина <Elizabethlarina679@gmail.com>
2026-03-15 21:07:29 +0700

    Implemented Taggable interface in NoteTagger

--- src/NoteTagger.java
+++ src/NoteTagger.java
@@ -1,2 +1,37 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class NoteTagger {
+    private Set<String> tags;
+
+    public NoteTagger() {
+        this.tags = new HashSet<>();
+    }
+
+    // добавляем тег
+    public void addTag(String tag) {
+        if (tag != null && !tag.isEmpty()) {
+            tags.add(tag.toLowerCase()); // в нижний регистр
+            System.out.println("Тег добавлен: " + tag);
+        }
+    }
+
+    // вывод всех тегов
+    public void showTags() {
+        System.out.println("Теги заметки: " + tags);
+    }
+
+    public static void main(String[] args) {
+        NoteTagger myNote = new NoteTagger();
+
+        // теги
+        myNote.addTag("Учеба");
+        myNote.addTag("Работа");
+        myNote.addTag("Важное");
+
+        // против дубликатов
+        myNote.addTag("Покупки");
+
+        myNote.showTags();
+    }
 }
