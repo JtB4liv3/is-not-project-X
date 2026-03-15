49aa1b6709b38f82a0ba5cd7c7ed62edb0c114f2
sayalszzz <tiktokbebra703@gmail.com>
2026-03-15 17:37:05 +0700

    my first code

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
-import java.util.Scanner;
-никита лох
+public class Main {
+    public static void main(String[] args) {
+        List<Note> notes = new ArrayList<>();
+        NoteReminder reminder = new NoteReminder(notes);
+
+        while (true) {
+            // Проверяем напоминания перед показом меню
+            reminder.checkReminders();
+
+
+            System.out.println("1. Создать заметку ");
+
+            System.out.println("11. Установить напоминание ");
+
+            Никита лох кстати
+        }
+    }
+}
