24b160e2effc8ef2203e298f6c4d05bbd4496d63
BogDeepSeek <kkorsakov47@gmail.com>
2026-03-15 17:07:31 +0700

    Создан метод NotePrinter метод main и добавленние хеш таблиц

--- src/NotePrinter.java
+++ src/NotePrinter.java
@@ -1,3 +1,14 @@
-public class NotePrinter {
+import java.util.ArrayList;
+import java.util.List;
 
+public class NotePrinter {
+    private List<String> notes;
+
+    public NotePrinter() {
+        notes = new ArrayList<>();
+
+    }
+    public static void main(String[] args) {
+        NotePrinter printer = new NotePrinter();
+    }
 }
