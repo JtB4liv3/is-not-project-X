a7da7740c8e5884b1075e8194db43aca3c9918ea
BogDeepSeek <kkorsakov47@gmail.com>
2026-03-15 17:22:16 +0700

    Созданы комментарии в классе

--- src/NotePrinter.java
+++ src/NotePrinter.java
@@ -1,25 +1,25 @@
-import java.util.ArrayList;
+import java.util.ArrayList; //библиотекм для заметок
 import java.util.List;
 
-public class NotePrinter {
-    private List<String> notes;
+public class NotePrinter { //Класс заметок
+    private List<String> notes; //Список для хранения заметок
 
-    public NotePrinter() {
+    public NotePrinter() { //функция созжающая примеры заметок
         notes = new ArrayList<>();
-        notes.add("Сделать работу с Git");
+        notes.add("Сделать работу с Git"); //сами примеры
         notes.add("Выучить Java");
     }
 
-    public void printAllNotes() {
+    public void printAllNotes() { //метод для вывода заметок в консоль
         System.out.println("Список всех заметок");
-        if (notes.isEmpty()) {
+        if (notes.isEmpty()) { //проверка на наличие заметок
             System.out.println("Заметок пока нет");
         }
 
     }
 
-    public static void main(String[] args) {
-        NotePrinter printer = new NotePrinter();
-        printer.printAllNotes();
+    public static void main(String[] args) { //главный метод
+        NotePrinter printer = new NotePrinter(); //создание объекта для вывода примеров
+        printer.printAllNotes(); //выполнение функции printAllNotes
     }
 }
